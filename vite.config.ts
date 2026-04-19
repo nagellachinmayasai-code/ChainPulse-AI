ebc6715792adecbe004d782929784287947c764f
nagellachinmayasai-code <nagellachinmayasai@gmail.com>
2026-04-19 22:11:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
 
     define: {
-      'import.meta.env.VITE_GEMINI_API_KEY'
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
 
     },
 
